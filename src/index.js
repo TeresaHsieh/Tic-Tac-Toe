10649e560c4773541466686dc4e252b628ff0ac3
Teresa Hsieh <teresahsieh850506@gmail.com>
2019-11-04 11:23:17 +0800

    Set Firebase Hosting

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom";
 import "./App.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import reducer from "./store/reducer";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 
-const store = createStore(reducer);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
